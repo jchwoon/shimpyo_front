6ef5636256ca54e5ffc9c036e5ff5625c5489540
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-05-26 22:24:07 +0900

    feat : 세팅 폴더 추가

---
+++ src/utils/Practice.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default function Practice() {
+  return (
+    <>
+      <StyledPrac />
+    </>
+  );
+}
+
+const StyledPrac = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: yellowgreen;
+`;
