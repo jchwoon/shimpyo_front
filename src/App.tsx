63add52b7384706c7f27053d2ff803ed74e38990
DESKTOP-4ST7896\toybo <toyboy0113@naver.com>
2023-06-13 23:21:35 +0900

    feat: 새로고침시 리프레쉬 토큰을 이용한 액세스 토큰 발급기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useSetRecoilState } from 'recoil';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { accessTokenAtom } from './recoil/atom';
+import { useEffect } from 'react';
+import { AxiosError } from 'axios';
+import useAuthorizedRequest from './hooks/useAuthorizedRequest';
+
+interface ResultData {
+  accessToken: string;
+}
 
 function App() {
+  const navigation = useNavigate();
+  const setAccessToken = useSetRecoilState(accessTokenAtom);
+
+  //권한 없을때 발생하는 함수
+  const handleUnAutorization = (error: AxiosError) => {
+    localStorage.setItem('isLoggedIn', JSON.stringify(false));
+    navigation('/');
+    console.error(error.message);
+  };
+
+  const { responseData, sendRequest } = useAuthorizedRequest<ResultData>({
+    onUnauthorized: handleUnAutorization,
+  });
+
+  useEffect(() => {
+    const sendRefreshToken = async () => {
+      if (!localStorage.getItem('isLoggedIn')) return;
+      await sendRequest({ url: '/api/refresh', withCredentials: true });
+
+      if (responseData && responseData.result) {
+        setAccessToken(responseData.result.accessToken);
+      }
+    };
+
+    sendRefreshToken();
+  }, [responseData, sendRequest, setAccessToken]);
   return (
     <>
       <RecoilRoot>
