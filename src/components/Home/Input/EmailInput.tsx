87cd70a930ba144bacc072d2b84184801fcb653d
DESKTOP-4ST7896\toybo <toyboy0113@naver.com>
2023-06-13 23:06:58 +0900

    feat: email 인풋 기능 추가

---
+++ src/components/Home/Input/EmailInput.tsx
@@ -0,0 +1,77 @@
+import { ChangeEvent, useState, useCallback, useEffect } from 'react';
+import { emailValidation } from '../../../utils/validation';
+import Input from '../../shared/UI/Input';
+import { useRecoilState } from 'recoil';
+import { emailValueAtom } from '../../../recoil/atom';
+import useHttpRequest from '../../../hooks/useHttpRequest';
+
+interface EmailInputProps {
+  getValid: (valid: boolean) => void;
+}
+
+export default function EmailInput({ getValid }: EmailInputProps) {
+  const { error, responseData, sendRequest } = useHttpRequest();
+
+  const [emailValue, setEmailValue] = useRecoilState(emailValueAtom);
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+
+  const validationCheck = emailValidation.test(emailValue);
+  const isValid = emailValue !== '' && !emailError;
+
+  const validateEmail = useCallback(() => {
+    if (!validationCheck) {
+      setEmailError(true);
+      setEmailErrorMessage('이메일 주소가 올바르지 않습니다.');
+    } else if (error) {
+      setEmailError(true);
+      setEmailErrorMessage('요청을 처리하는 동안 문제가 발생했습니다.');
+    } else if (responseData && !responseData.isSuccess) {
+      setEmailError(true);
+      setEmailErrorMessage('이미 가입된 이메일 주소입니다.');
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+  }, [validationCheck, error, responseData]);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmailValue(value);
+  };
+
+  const handleCheckEmail = async () => {
+    if (emailValue && validationCheck) {
+      await sendRequest({ url: `/api/check-email?email=${emailValue}`, method: 'POST' });
+    }
+  };
+
+  useEffect(() => {
+    const overlapTest = setTimeout(() => {
+      handleCheckEmail();
+    }, 700);
+
+    return () => clearTimeout(overlapTest);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [emailValue, validationCheck]);
+
+  useEffect(() => {
+    if (!emailValue) {
+      setEmailErrorMessage('이메일을 입력해주세요');
+      return;
+    }
+
+    validateEmail();
+    getValid(isValid);
+  }, [emailValue, validateEmail, getValid, isValid]);
+  return (
+    <Input
+      value={emailValue}
+      error={emailError}
+      errorMessage={emailErrorMessage}
+      onChange={handleEmailChange}
+      placeholder="이메일"
+      type="email"
+    />
+  );
+}
