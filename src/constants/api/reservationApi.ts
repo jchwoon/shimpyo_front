4185c059a593588f2065663439b760520e3ded95
John <johnpk1007@gmail.com>
2023-07-20 18:20:30 +0900

    fix: minor error

--- src/constants/api/reservationApi.ts
+++ src/constants/api/reservationApi.ts
@@ -1,3 +1,4 @@
 export const RESERVATION_PREPARE_API_PATH = '/user/reservations/prepare';
 export const MEMBER_RESERVATION_API_PATH = '/user/reservations';
 export const NON_MEMBER_RESERVATION_API_PATH = '/api/non-member-reservations';
+export const NON_MEMBER_RESERVATION_TEXT_API_PATH = '/api/certification/non-member'
